0265f61b4b727a2728c1d001aec8176eae8039d4
murajsam <128522266+murajsam@users.noreply.github.com>
2024-01-19 13:32:18 +0100

    solution

---
+++ BusinessLayer/MenuItemBusiness.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Models;
+
+namespace BusinessLayer
+{
+    public class MenuItemBusiness
+    {
+        private readonly MenuItemRepository menuItemRepository;
+
+        public MenuItemBusiness()
+        {
+            menuItemRepository = new MenuItemRepository();
+        }
+
+        //Vraca spisak svih stavki iz jelovnika
+        public List<MenuItem> GetAllMenuItems()
+        {
+            return menuItemRepository.GetAllMenuItems();
+        }
+
+        public List<MenuItem> GetAllMenuItemsFromRange(double priceFrom, double priceTo)
+        {
+            List<MenuItem> filteredMenuItems = menuItemRepository.GetAllMenuItems()
+            .Where(menuItem => menuItem.price > priceFrom && menuItem.price < priceTo)
+            .ToList();
+
+            return filteredMenuItems;
+
+        }
+
+        public bool InsertMenuItem(MenuItem menuItem) 
+        { 
+            return menuItemRepository.InsertMenuItem(menuItem);
+        }
+    }
+}
